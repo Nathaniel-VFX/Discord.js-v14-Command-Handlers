49bf754293c8287005fe26f3b6ff6600d06e6a6a
Nathaniel <nathaniel.vfx.official@gmail.com>
2022-03-30 17:39:30 +0800

    Updated correct type of activity status
    Playing is type 1
    Listening is type 2
    Watching is type 3
    Competing is type 5

--- events/ready.js
+++ events/ready.js
@@ -4,8 +4,8 @@ const chalk = require('chalk')
 client.on("ready", () => {
 	const activities = [
 		{ name: `${client.channels.cache.size} Channels`, type: 1 }, // PLAYING
-		{ name: `${client.users.cache.size} Users`, type: 3 }, // WATCHING
 		{ name: `${client.guilds.cache.size} Servers`, type: 2 }, // LISTENING
+		{ name: `${client.users.cache.size} Users`, type: 3 }, // WATCHING
 		{ name: `Discord.js v14`, type: 5 } // COMPETING
 	];
 	const status = [
