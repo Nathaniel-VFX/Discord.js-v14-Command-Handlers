ea739496ecca5f56b6b6931a982af1572d67d74a
Nathaniel <nathaniel.vfx.official@gmail.com>
2022-06-02 22:51:38 +0800

    Example of how to do autocomplete slash command.
    Example of how to do autocomplete slash command with this handler.

---
+++ slashCommands/fun/animal.js
@@ -0,0 +1,34 @@
+const { EmbedBuilder, ApplicationCommandType } = require('discord.js');
+
+module.exports = {
+	name: 'animal',
+	description: "A test command for showing autocomplete slash command.",
+	cooldown: 3000,
+	type: ApplicationCommandType.ChatInput,
+	options: [
+        {
+            name: 'animal',
+            description: 'Choose an animal.',
+            type: 3,
+            required: true,
+            autocomplete: true
+        }
+    ],
+    autocomplete: (interaction, choices) => {
+        const data = {
+            '908385923673231481': ['dog', 'cat', 'rabbit'],
+            '967751631695405097': ['lizard', 'horse', 'hamster']
+        }
+        data[interaction.guild.id].forEach(animal => {
+            choices.push({
+                name: `${animal}`,
+                value: `${animal}`
+            });
+        });
+        interaction.respond(choices).catch(console.error);
+    },
+	run: async (client, interaction) => {
+        const animal = interaction.options.get('animal').value;
+        interaction.reply({ content: `${interaction.user}, You chose **${animal}**!` });
+	}
+};
